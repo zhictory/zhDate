d307f77260a387606fa118549283a0222f9cd359
zhangzhensheng <zhangzhensheng@heungkong.com>
2018-06-25 12:41:17 +0800

    Updated 更新 webpack 配置

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const UglifyPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        use: 'babel-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new UglifyPlugin()
+  ]
+
+};
